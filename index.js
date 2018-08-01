1b6e23a505eb2e31adf791d0431f287cfe86b253
FOUADGRITLI@YAHOO.COM <fouadgritli@yahoo.com>
2018-08-01 16:45:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Fouad"
-var height = 178
+var height = "178"
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:"+height+1)
+console.log("Height:"+parseInt(height+1))
 console.log(height)
